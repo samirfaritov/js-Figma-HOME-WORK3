432541ec3ce3b01ee63fc03d8d4590b1935a8d96
samirfaritov <samirfaritov6@gmail.com>
2023-11-21 17:58:09 +0500

    js Figma HOME WORK @

---
+++ main.js
@@ -0,0 +1,42 @@
+let okno = document.querySelector('.okno')
+let box = document.querySelector('.box')
+let svg = document.querySelector('.svg')
+let bx = document.querySelector('.bx')
+let menyu = document.querySelector('.menyu')
+let svg1 = document.querySelector('.svg1')
+let cont = document.querySelector('.conteiner')
+let b = document.querySelector('.b')
+
+
+okno.addEventListener('click', () => {
+    box.style.display = 'block'
+    document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.47)'
+})
+
+svg.addEventListener('click', () => {
+    box.style.display = 'none'
+    document.body.style.backgroundColor = '#fff'
+})
+
+// b.addEventListener('click', (e) => {
+    
+//     console.log(e.target);
+//     if (e.target === b) {
+//         box.style.display = 'block'
+//         console.log('click');
+//         document.body.style.overflow = ''
+//     }
+// })
+
+bx.addEventListener('click', () => {
+    menyu.style.positionAbsolute
+    menyu.style.right = '0'
+    bx.style.display = 'none'
+})
+
+svg1.addEventListener('click', () => {
+    menyu.style.positionAbsolute
+    menyu.style.right = '-456px'
+    bx.style.display = 'block'
+})
+
