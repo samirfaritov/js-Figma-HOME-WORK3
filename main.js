9011832db61cc53c5ffc457fb9961e03f21194bc
samirfaritov <samirfaritov6@gmail.com>
2023-11-23 14:49:11 +0500

    Okno

--- main.js
+++ main.js
@@ -11,11 +11,14 @@ let b = document.querySelector('.b')
 okno.addEventListener('click', () => {
     box.style.display = 'block'
     document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.47)'
+    okno.style.filter = 'blur(2px)'
 })
 
 svg.addEventListener('click', () => {
     box.style.display = 'none'
     document.body.style.backgroundColor = '#fff'
+    okno.style.filter = 'blur()'
+
 })
 
 // b.addEventListener('click', (e) => {
@@ -32,11 +35,13 @@ bx.addEventListener('click', () => {
     menyu.style.positionAbsolute
     menyu.style.right = '0'
     bx.style.display = 'none'
+    okno.style.filter = 'blur(2px)'
 })
 
 svg1.addEventListener('click', () => {
     menyu.style.positionAbsolute
     menyu.style.right = '-500px'
     bx.style.display = 'block'
+    okno.style.filter = 'blur()'
 })
 
